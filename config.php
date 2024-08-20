25e5b7ffedbc1f9df6af170c1b9f0456d6dfae39
Guy Thierry Charles <charlesguythierry@gmail.com>
2024-08-20 14:09:56 -0400

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Database configuration
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'webserver');
-define('DB_PASSWORD', 'MyP@ssW@rd1');
-define('DB_NAME', 'bug_reports');
-define('DB_SOCKET', '/var/run/mysqld/mysqld.sock');
