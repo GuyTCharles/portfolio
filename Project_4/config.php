7d8069bb6b03154b00c1d3cfaafa04cd63f97281
Guy Thierry Charles <charlesguythierry@gmail.com>
2024-08-20 14:18:47 -0400

    Create config.php

---
+++ Project_4/config.php
@@ -0,0 +1,7 @@
+<?php
+// Database configuration
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'webserver');
+define('DB_PASSWORD', 'MyP@ssW@rd1');
+define('DB_NAME', 'bug_reports');
+define('DB_SOCKET', '/var/run/mysqld/mysqld.sock');
